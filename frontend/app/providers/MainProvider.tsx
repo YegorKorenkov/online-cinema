e2bd5ed52000d56c5cb06d391977fc0663f37abe
YegorKorenkov <eg.korenkov@gmail.com>
2022-08-17 12:46:03 +0300

    add(redux, toast): add redux-toolkit, toastr

--- frontend/app/providers/MainProvider.tsx
+++ frontend/app/providers/MainProvider.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 
 import Layout from '@/components/layout/Layout';
+
+import { store } from '@/store/store';
+
+import ReduxToast from './ReduxToast';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -17,9 +22,12 @@ interface Props {
 
 const MainProvider: FC<Props> = ({ children }) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Layout>{children}</Layout>
-		</QueryClientProvider>
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<ReduxToast />
+				<Layout>{children}</Layout>
+			</QueryClientProvider>
+		</Provider>
 	);
 };
 
